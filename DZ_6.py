de364089df3f7c6bf14a55d19e7bf256c6670f7e
GodartSTARK <122571057+GodartSTARK@users.noreply.github.com>
2023-04-04 14:47:20 +0300

    Update DZ_6.py

--- DZ_6.py
+++ DZ_6.py
@@ -2,6 +2,8 @@ import transliterate
 import os
 import pathlib
 import shutil
+import patoolib
+import tempfile
 
 
 def normalize(filename):
@@ -17,28 +19,52 @@ def move(file_path):
         if file_extension in allowed_extensions:
             folder_path = pathlib.Path(folder_name)
             folder_path.mkdir(exist_ok=True)
-            new_file_path = folder_path / normalize(file_path.name)
-            shutil.move(str(file_path), str(new_file_path))
-            #print(f"Moved {file_path} to {new_file_path}")
-            return 
+            if file_extension in dict_folders['archives']:
+                temp_dir = pathlib.Path(tempfile.mkdtemp())
+                try:
+                    patoolib.extract_archive(str(file_path), outdir=str(temp_dir))
+                    for extracted_file in temp_dir.glob('**/*'):
+                        if extracted_file.is_file():
+                            extracted_file_extension = os.path.splitext(extracted_file)[-1].lower()[1:]
+                            for folder_name, allowed_extensions in dict_folders.items():
+                                if extracted_file_extension in allowed_extensions:
+                                    new_file_path = pathlib.Path(folder_name) / normalize(extracted_file.name)
+                                    shutil.move(str(extracted_file), str(new_file_path))
+                                    print(f"Moved {extracted_file} to {new_file_path}")
+                                    break
+                            else:
+                                new_file_path = pathlib.Path('other') / normalize(extracted_file.name)
+                                shutil.move(str(extracted_file), str(new_file_path))
+                                print(f"Moved {extracted_file} to {new_file_path}")
+                except Exception as e:
+                    print(f"Failed to extract {file_path}: {e}")
+                finally:
+                    shutil.rmtree(str(temp_dir))
+            else:
+                new_file_path = folder_path / normalize(file_path.name)
+                shutil.move(str(file_path), str(new_file_path))
+                print(f"Moved {file_path} to {new_file_path}")
+                break
+    else:
+        new_file_path = pathlib.Path('other') / normalize(file_path.name)
+        shutil.move(str(file_path), str(new_file_path))
+        print(f"Moved {file_path} to {new_file_path}")
+
 
 dict_folders = {
     'images': ['jpeg', 'png', 'jpg', 'svg'],
     'video': ['avi', 'mp4', 'mov', 'mkv'],
     'documents': ['doc', 'docx', 'txt', 'pdf', 'xlsx', 'pptx'],
     'audio': ['mp3', 'ogg', 'wav', 'amr'],
-    'archives': ['zip', 'gz', 'tar'],
+    'archives': ['zip', 'tar', 'gz', 'bz2'],
     'other': []         
 }
 
-
 source_folder = pathlib.Path(r'd:\testfolder')
-
 
 for folder_name in dict_folders.keys():
     folder_path = pathlib.Path(folder_name)
     folder_path.mkdir(exist_ok=True)
-
 
 for file_path in source_folder.glob('**/*'):
     if file_path.is_file():
